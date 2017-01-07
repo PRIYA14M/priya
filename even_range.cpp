07c166d3aceea23798656d4cc6dfe1329f713920
PRIYADHARSHINI <mpriyadharshini46@gmail.com>
2017-01-07 20:31:16 +0530

    Add files via upload

---
+++ even_range.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int start,final;
+cout<<"Enter a starting and ending values"<<endl;
+cin>>start>>final;
+cout<<"Odd numbers are "<<endl;
+for(int i=start;i<=final;i++)
+{
+if(i%2 == 0)
+cout<<i<<endl;
+}
+return(0);
+}
