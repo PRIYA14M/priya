d1f9208350fbcf079892d5f2f54803043c1a65ac
PRIYADHARSHINI <mpriyadharshini46@gmail.com>
2017-01-07 20:17:08 +0530

    Update digits_count.cpp

--- digits_count.cpp
+++ digits_count.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-int num,data,count=0;
-cout<<"Enter a num"<<endl;
-cin>>num;
-data=num;
+int n,d,c=0;
+cout<<"Enter a n"<<endl;
+cin>>n;
+d=n;
 do
 {
-++count;
-num = num / 10;
-}while(num!=0);
-cout<<data<<" is "<<count<<" digits";
+++c;
+n = n / 10;
+}while(n!=0);
+cout<<d<<" is "<<c<<" digits";
 return(0);
 }
