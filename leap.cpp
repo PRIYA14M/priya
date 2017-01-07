5f1e144c5f7c89cebdbb1ab2618f5e240908be2e
PRIYADHARSHINI <mpriyadharshini46@gmail.com>
2017-01-07 20:27:06 +0530

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-int year;
+int yr;
 cout<<"Enter a year"<<endl;
-cin>>year;
-if(year % 4 == 0)
-cout<<year<<" is a leap year";
+cin>>yr;
+if(yr % 4 == 0)
+cout<<yr<<" is a leap year";
 else
-cout<<year<<" is not a leap year";
+cout<<yr<<" is not a leap year";
 return(0);
 }
