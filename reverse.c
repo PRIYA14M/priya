14651d31f3cb0bf1eafaaaf0028e621cf0f472f2
PRIYADHARSHINI <mpriyadharshini46@gmail.com>
2017-01-09 18:53:04 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+ 
+int main()
+{
+   char i[100];
+  
+   printf("Enter a string to reverse\n");
+   gets(i);
+   strrev()  
+   strrev(i);
+   printf("Reversed string is: %s\n", i);
+   getch();
+   return 0;
+}
